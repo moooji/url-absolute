fba504af775741c9f43d42cf255f6992e5159f37
moooji <steffen.straetz@gmail.com>
2016-06-18 23:24:50 +0200

    Upgraded dependencies

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const url = require('url');
-const validation = require('valido');
+const is = require('valido');
 
 function absolutify(baseUrl, partialUrl) {
-  if (!validation.isUrl(baseUrl)) {
+  if (!is.uri(baseUrl)) {
     throw new TypeError('Invalid base URL');
   }
 
-  if (!validation.isString(partialUrl)) {
+  if (!is.string(partialUrl)) {
     throw new TypeError('Invalid partial URL');
   }
 
-  if (validation.isUrl(partialUrl)) {
+  if (is.uri(partialUrl)) {
     return partialUrl;
   }
 
